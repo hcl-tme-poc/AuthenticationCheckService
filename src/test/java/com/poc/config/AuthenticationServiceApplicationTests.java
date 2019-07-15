aefbf33b0efad603b4398678dd960e03497d4d78
Nataraj kotambari <nkotambari@tracfone.com>
2019-07-15 12:27:35 +0530

    Test case commented

--- src/test/java/com/poc/config/AuthenticationServiceApplicationTests.java
+++ src/test/java/com/poc/config/AuthenticationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.poc.config;
+/*package com.poc.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class AuthenticationServiceApplicationTests {
 	}
 
 }
+*/
